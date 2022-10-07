6ea1022eea30940c39dc8a13e8803ccd8a0b47f3
shrishail <shrikariagr92@gmail.com>
2022-10-07 09:31:04 +0530

    JS Program

--- factorial.js
+++ factorial.js
@@ -1,25 +1,14 @@
-// program to find the factorial of a number
-function factorial(x) {
+let n = 4;
+let f =1;
+if(n==0 | n==1 ){
+    f = 1;
+    console.log(f);
 
-    // if number is 0
-    if (x == 0) {
-        return 1;
-    }
-
-    // if number is positive
-    else {
-        return x * factorial(x - 1);
-    }
 }
+else{
+    for(var i = n; i>=1;i++){
+            f = f * i;
+    }
+    console.log(f);
 
-// take input from the user
-const num = prompt('Enter a positive number: ');
-
-// calling factorial() if num is positive
-if (num >= 0) {
-    const result = factorial(num);
-    console.log(`The factorial of ${num} is ${result}`);
-}
-else {
-    console.log('Enter a positive number.');
 }
---
+++ haromic.js
@@ -0,0 +1,6 @@
+var num = 6;
+var sum=0;
+for(var i=1;i<=num;i++){
+    sum=sum+1/i;
+}
+console.log("N th Harmonic number is " +sum);
